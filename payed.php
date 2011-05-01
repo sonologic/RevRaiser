dd427048d893eb3cf1f949e1a7144858e428755c
Sonologic <gmc@sonologic.nl>
2011-05-01 20:35:15 +0200

    More gpl boilerplate

--- payed.php
+++ payed.php
@@ -7,20 +7,20 @@
 **
 **    Copyright (c) 2011 by Koen Martens
 **
-**    This file is part of Foobar.
+**    This file is part of RevRaiser.
 **
-**    Foobar is free software: you can redistribute it and/or modify
+**    RevRaiser is free software: you can redistribute it and/or modify
 **    it under the terms of the GNU General Public License as published by
 **    the Free Software Foundation, either version 3 of the License, or
 **    (at your option) any later version.
 **
-**    Foobar is distributed in the hope that it will be useful,
+**    RevRaiser is distributed in the hope that it will be useful,
 **    but WITHOUT ANY WARRANTY; without even the implied warranty of
 **    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 **    GNU General Public License for more details.
 **
 **    You should have received a copy of the GNU General Public License
-**    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+**    along with RevRaiser.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 function render_title($session_data) {
